9ca5f0cf2d566ce7f809de4e3fa5fe5c82d3169c
Vaibhav Chandrabhan Pawar <VAIBHAVCPAWAR43388@GMAIL.COM>
2023-09-14 18:48:26 +0530

    Length of last word

---
+++ Codes/All codes/LengthoflastWord14-9.cs
@@ -0,0 +1,53 @@
+﻿/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.*/
+
+using System;
+
+public class Lengthword
+{
+    public int LengthOfLastWord(string s)
+    {
+        bool a = false;
+        int len = 0;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (char.IsLetter(s[i]))
+            {
+                a = true;
+                len++;
+            }
+            else
+            {
+                if (a == true)
+                {
+                    return len;
+                }
+            }
+
+        }
+        return len;
+    }
+
+}
+/*public static void main(string []args)
+    {
+    string input = "we are achievers";
+    Lengthword obj = new Lengthword();
+    Console.WriteLine("The length of last word is "+ obj.LengthOfLastWord(input) );
+        Console.WriteLine("Enter a string");
+        string a = Console.ReadLine();
+        obj.LengthOfLastWord(a);
+        Console.WriteLine("The length of last word is"+ obj.LengthOfLastWord(a) );
+    }
+}*/
---
+++ Codes/All codes/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//palindrome
+
+Console.WriteLine("\n >>>> To Find a Number is Palindrome or not <<<< ");
+Console.Write("\n Enter a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+var x = new Palindrome();
+x.IsPalindrome(n);
+
+//Length of last word
+
+string input = "we are achievers";
+Lengthword obj = new Lengthword();
+Console.WriteLine("The length of last word is " + obj.LengthOfLastWord(input));
+
+Lengthword obj1 = new Lengthword();
+Console.WriteLine("Enter a string");
+string a = Console.ReadLine();
+obj1.LengthOfLastWord(a);
+Console.WriteLine("The length of last word is" + obj1.LengthOfLastWord( a) );
